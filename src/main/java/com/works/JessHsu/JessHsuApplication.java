a6b556f9e9208e4d3f71fdef4a58f90cf44af489
Jingjhih97 <ian021130@gmail.com>
2025-10-15 21:43:03 +0800

    Initial commit - Spring Boot project setup

---
+++ src/main/java/com/works/JessHsu/JessHsuApplication.java
@@ -0,0 +1,13 @@
+package com.works.JessHsu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JessHsuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JessHsuApplication.class, args);
+	}
+
+}
